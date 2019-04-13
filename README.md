3a3a80e527b47204fe13a8e4de455de1c5f2ce9b
Albrecht Köhnlein <ak@koehnlein.eu>
2019-04-13 15:39:46 +0200

    Update README file

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
-Alpine with composer, npm, yarn, gulp, typo3/surf etc.
+Multi functional images for unit tests and deployment in your CI pipeline.
+
+Includes pre installed
+
+* composer
+* nodejs
+* npm
+* yarn
+* gulp
+* TYPO3/Surf
+* xdebug
+* php-mysqli
+* ...
 
 
-Choose your PHP version by using the matching tag:
+Choose your PHP version and OS by using the matching tag:
 
-* `akoehnlein/surf:php56` PHP 5.6
-* `akoehnlein/surf:php70` PHP 7.0
-* `akoehnlein/surf:php71` PHP 7.1
-* `akoehnlein/surf:php72` PHP 7.2
+* `akoehnlein/surf:php73` PHP 7.3, Alpine
+* `akoehnlein/surf:php73-debian` PHP 7.3, Debian
+* `akoehnlein/surf:php72` PHP 7.2, Alpine
+* `akoehnlein/surf:php72-debian` PHP 7.2, Debian
+* `akoehnlein/surf:php71` PHP 7.1, Alpine
+* `akoehnlein/surf:php71-debian` PHP 7.1, Debian
+* `akoehnlein/surf:php70` PHP 7.0, Alpine
+* `akoehnlein/surf:php56` PHP 5.6, Alpine
 
 All available tags are listed on <https://hub.docker.com/r/akoehnlein/surf/tags/>
--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-docker build --pull . -f php56/Dockerfile -t akoehnlein/surf:php56 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php56 /tmp/test.sh && \
-docker build --pull . -f php70/Dockerfile -t akoehnlein/surf:php70 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php70 /tmp/test.sh && \
+docker build --pull . -f php73-debian/Dockerfile -t akoehnlein/surf:php73-debian && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php73-debian /tmp/test.sh && \
+docker build --pull . -f php73/Dockerfile -t akoehnlein/surf:php73 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php73 /tmp/test.sh \
+docker build --pull . -f php72-debian/Dockerfile -t akoehnlein/surf:php72-debian && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php72-debian /tmp/test.sh && \
+docker build --pull . -f php72/Dockerfile -t akoehnlein/surf:php72 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php72 /tmp/test.sh \
+docker build --pull . -f php71-debian/Dockerfile -t akoehnlein/surf:php71-debian && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php71-debian /tmp/test.sh && \
 docker build --pull . -f php71/Dockerfile -t akoehnlein/surf:php71 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php71 /tmp/test.sh && \
-docker build --pull . -f php72/Dockerfile -t akoehnlein/surf:php72 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php72 /tmp/test.sh
+docker build --pull . -f php70/Dockerfile -t akoehnlein/surf:php70 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php70 /tmp/test.sh && \
+docker build --pull . -f php56/Dockerfile -t akoehnlein/surf:php56 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php56 /tmp/test.sh && \
+echo "done"
