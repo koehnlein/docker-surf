d25638d855aa23a0833b6694f63bd9db02c8d24a
Albrecht Köhnlein <albrecht.koehnlein@verdure.de>
2020-10-26 17:55:20 +0100

    Add akoehnlein/surf:php74

--- build.sh
+++ build.sh
@@ -4,8 +4,9 @@ docker build --pull . -f debian/php74/Dockerfile -t akoehnlein/surf:php74-debian
 docker build --pull . -f debian/php73/Dockerfile -t akoehnlein/surf:php73-debian && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php73-debian /tmp/test.sh && \
 docker build --pull . -f debian/php72/Dockerfile -t akoehnlein/surf:php72-debian && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php72-debian /tmp/test.sh && \
 docker build --pull . -f debian/php71/Dockerfile -t akoehnlein/surf:php71-debian && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php71-debian /tmp/test.sh && \
-docker build --pull . -f alpine/php73/Dockerfile -t akoehnlein/surf:php73 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php73 /tmp/test.sh \
-docker build --pull . -f alpine/php72/Dockerfile -t akoehnlein/surf:php72 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php72 /tmp/test.sh \
+docker build --pull . -f alpine/php74/Dockerfile -t akoehnlein/surf:php74 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php74 /tmp/test.sh && \
+docker build --pull . -f alpine/php73/Dockerfile -t akoehnlein/surf:php73 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php73 /tmp/test.sh && \
+docker build --pull . -f alpine/php72/Dockerfile -t akoehnlein/surf:php72 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php72 /tmp/test.sh && \
 docker build --pull . -f alpine/php71/Dockerfile -t akoehnlein/surf:php71 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php71 /tmp/test.sh && \
 docker build --pull . -f alpine/php70/Dockerfile -t akoehnlein/surf:php70 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php70 /tmp/test.sh && \
 docker build --pull . -f alpine/php56/Dockerfile -t akoehnlein/surf:php56 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php56 /tmp/test.sh && \
