be23c5b80b3c344ade1ffda113f98cf442d1c1bf
Albrecht Köhnlein <ak@koehnlein.eu>
2021-11-06 15:01:39 +0100

    Disable support for older packages

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ docker build --pull . -f alpine/php74/Dockerfile -t akoehnlein/surf:php74 && doc
 docker build --pull . -f alpine/php73/Dockerfile -t akoehnlein/surf:php73 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php73 /tmp/test.sh && docker push akoehnlein/surf:php73 && \
 docker build --pull . -f alpine/php72/Dockerfile -t akoehnlein/surf:php72 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php72 /tmp/test.sh && docker push akoehnlein/surf:php72 && \
 docker build --pull . -f alpine/php71/Dockerfile -t akoehnlein/surf:php71 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php71 /tmp/test.sh && docker push akoehnlein/surf:php71 && \
-docker build --pull . -f alpine/php70/Dockerfile -t akoehnlein/surf:php70 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php70 /tmp/test.sh && docker push akoehnlein/surf:php70 && \
-docker build --pull . -f alpine/php56/Dockerfile -t akoehnlein/surf:php56 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php56 /tmp/test.sh && docker push akoehnlein/surf:php56 && \
+#docker build --pull . -f alpine/php70/Dockerfile -t akoehnlein/surf:php70 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php70 /tmp/test.sh && docker push akoehnlein/surf:php70 && \
+#docker build --pull . -f alpine/php56/Dockerfile -t akoehnlein/surf:php56 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh akoehnlein/surf:php56 /tmp/test.sh && docker push akoehnlein/surf:php56 && \
 echo "done"
